5b641aefeb75b06ae16ccdbd8c67c97c349f3f36
Samuel Mårtensson <samuel137@live.se>
2021-12-04 16:00:09 +0100

    reduce calls to rtdb

--- proxy.js
+++ proxy.js
@@ -1,35 +1,32 @@
-require('dotenv').config({ path: __dirname + '/.env' });
-const fetch = require('node-fetch');
-const express = require('express');
+require("dotenv").config({ path: __dirname + "/.env" });
+const express = require("express");
 const app = express();
 const port = 3001;
-const firebase = require('firebase-admin');
-const serviceAccount = require('./serviceaccountcred');
+const firebase = require("firebase-admin");
+const serviceAccount = require("./serviceaccountcred");
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DB_URL,
 });
 
-app.get('/', async (req, res) => {
-  const { instanceId } = req.query;
+app.get("/", async (req, res) => {
+  const { instanceId, orgId } = req.query;
 
   firebase
     .database()
-    .ref('/')
-    .once('value', (snapshot) => {
-      const instanceItems = Object.values(snapshot.val()).find((item) =>
-        Object.keys(item).find((key) => key === instanceId)
-      )[instanceId];
+    .ref(`/${orgId}/${instanceId}`)
+    .once("value", (snapshot) => {
+      const instanceItems = snapshot.val();
 
       res.json(
         Object.values(instanceItems).filter(
-          (item) => item['render-status'] === 'ready'
+          (item) => item["render-status"] === "ready"
         )
       );
     })
     .catch(() => {
-      res.send({ error: { message: 'Something went wrong in proxy' } });
+      res.send({ error: { message: "Something went wrong in proxy" } });
     });
 });
 
