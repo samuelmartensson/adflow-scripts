4ea9faf972ee9a749823a84c6230cbe80e91ae62
Samuel Mårtensson <samuel137@live.se>
2022-01-15 21:48:39 +0100

    It was the postrender.js LOOOOL

--- local/postrender.js
+++ local/postrender.js
@@ -18,30 +18,34 @@ module.exports = (job, settings, action) => {
   const uid = job.uid;
 
   return new Promise((resolve, reject) => {
-    for (let index = 0; index < data.itemCount; index++) {
-      const { id } = data.items[index];
-      const rd = fs.createReadStream(
-        `${rootUserPath}/Desktop/nexrender_cli/Temp/${uid}/` +
-          generateResultName(index)
-      );
-      const wr = fs.createWriteStream(
-        `${rootUserPath}/Desktop/nexrender_cli/renders/${id}.jpg`
-      );
+    const promises = [...Array(data.itemCount).keys()].map((index) => {
+      return new Promise((innerResolve) => {
+        const { id } = data.items[index];
+        const rd = fs.createReadStream(
+          `${rootUserPath}/Desktop/nexrender_cli/Temp/${uid}/` +
+            generateResultName(index)
+        );
+        const wr = fs.createWriteStream(
+          `${rootUserPath}/Desktop/nexrender_cli/renders/${id}.jpg`
+        );
 
-      try {
-        rd.on("error", reject);
-        wr.on("error", reject);
-        wr.on("finish", () => {
-          if (index === data.itemCount - 1) {
-            resolve(job);
-          }
-        });
-        rd.pipe(wr);
-      } catch (error) {
-        rd.destroy();
-        wr.end();
-        throw error;
-      }
-    }
+        try {
+          rd.on("error", reject);
+          wr.on("error", reject);
+          wr.on("finish", () => {
+            innerResolve();
+          });
+          rd.pipe(wr);
+        } catch (error) {
+          rd.destroy();
+          wr.end();
+          throw error;
+        }
+      });
+    });
+
+    Promise.all(promises).then(() => {
+      resolve(job);
+    });
   });
 };
