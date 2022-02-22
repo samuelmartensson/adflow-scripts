cc94d0c39789e4e777ed8141bc549685fbf45681
Samuel Mårtensson <samuel137@live.se>
2022-02-22 22:12:40 +0100

    fix fetchQueueData return

--- proxy.js
+++ proxy.js
@@ -1,26 +1,27 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const firebase = require("firebase-admin");
 
-const fetchQueueData = async (instanceId) => {
+const fetchQueueData = async (instanceId = "i-0d343d22f305f353e") => {
   if (!instanceId) return Promise.reject();
 
-  firebase
-    .database()
-    .ref(instanceId)
-    .once("value", (snapshot) => {
-      const instanceItems = snapshot.val();
+  return new Promise((resolve, reject) => {
+    firebase
+      .database()
+      .ref(instanceId)
+      .once("value", (snapshot) => {
+        const instanceItems = snapshot.val();
+        if (!instanceItems) return [];
 
-      if (!instanceItems) return [];
-
-      return (
-        Object.values(instanceItems).filter(
-          (item) => item?.["render-status"] === "ready"
-        ) || 0
-      );
-    })
-    .catch(() => {
-      return Promise.reject();
-    });
+        resolve(
+          Object.values(instanceItems).filter(
+            (item) => item?.["render-status"] === "ready"
+          ) || 0
+        );
+      })
+      .catch(() => {
+        reject();
+      });
+  });
 };
 
 module.exports = { fetchQueueData };
